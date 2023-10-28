7384f71f12392575b7449f29f597dbbfa6cb2783
anushkaaa34 <133742135+anushkaaa34@users.noreply.github.com>
2023-10-28 10:03:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const exampleBtn = document.getElementsByClassName('d1');
+
+exampleBtn.addEventListener('click', () => {
+  window.open('index.html', '_blank');
+});
+
+const exampleBtn2 = document.getElementsByClassName('d2');
+
+exampleBtn.addEventListener('click', () => {
+  window.open('dashboard.html', '_blank');
+});
